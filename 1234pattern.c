82ba10d1153258b268090ac0dfb98aeb6f55d3d9
NehaThorat19 <hnehathorat7@gmail.com>
2021-04-18 21:51:49 +0530

    Number pattern printing in C

---
+++ 1234pattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void fun()
+{
+	
+
+int r,c;
+	for(r=1;r<5;r++)
+	{
+		for(c=1;c<5;c++)
+		{
+			printf(" %d ",r);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	fun();
+		return 0;
+}
